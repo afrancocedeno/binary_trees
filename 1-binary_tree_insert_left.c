55a806188725fde8c95adfeb663a3c019b030daf
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-08 18:40:57 -0500

    code simplified by just one if statement

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 
 /**
- *
- *
+ * @brief 
+ * 
+ * @parent:
+ * @value:
+ * Return: binary_tree_t* 
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
@@ -12,26 +15,20 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-
-	if (parent->left == NULL)
+	/* both cases, node needs to be created */
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+	/* replace it if left-child exists */
+	if ((*parent).left)
 	{
-		parent->left = binary_tree_node(parent, value);
-		if (parent->left == NULL)
-			return (NULL);
-		return (parent->left);
-	}
-	else
-	{
-		new_node = binary_tree_node(parent, value);
-		if (new_node == NULL)
-			return (NULL);
+		/* reference the left child */
 		new_node->left = parent->left;
 		/* The previous node of the child must be the new node */
 		parent->left->parent = new_node;
-		parent->left = new_node;
-
-		return (new_node);
 	}
+	/* Both cases refer the new node as left child to parent */
+	parent->left = new_node;
 
-	return (NULL);
+	return (new_node);
 }
