8c6009efd2bebed5c460b2d50634e508defe93ec
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-08 18:34:33 -0500

    Previous node of the child must be the new node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -26,7 +26,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		if (new_node == NULL)
 			return (NULL);
 		new_node->left = parent->left;
+		/* The previous node of the child must be the new node */
+		parent->left->parent = new_node;
 		parent->left = new_node;
+
 		return (new_node);
 	}
 
