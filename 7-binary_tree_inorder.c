fa8be992aaf6389ea42a4c968b27e307cc81a47e
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-09 15:05:49 -0500

    file create single call to function func

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_inorder - function that in-order traversal a BT.
+ * @tree: pointer to the root of the tree to traverse.
+ * @func: pointer to a function to call for each node.
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	/* now recursion right subtree */
+	if ((*tree).left)
+		binary_tree_inorder((*tree).left, func);
+	/* visit the root */
+	func((*tree).n);
+	/* now recursion right subtree */
+	if ((*tree).right)
+		binary_tree_inorder((*tree).right, func);
+}
