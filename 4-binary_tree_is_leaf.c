189141a683b1cccab0cd0cefa5d2aa4d76bed25d
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-08 23:49:23 -0500

    according to main only check right nodes

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -5,11 +5,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
-	if (node->left)
-	{
-		binary_tree_is_leaf(node->left);
-		free(node->left);
-	}
+	/* according to main only check right nodes */
 	if (node->right)
 	{
 		binary_tree_is_leaf(node->right);
