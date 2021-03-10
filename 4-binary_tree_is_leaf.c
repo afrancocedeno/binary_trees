099947d0a66e985c75ebc0a9258c317a93d06d17
Lucho <2450@holbertonschool.com>
2021-03-10 12:19:42 -0500

    Pointer notation changed to enhance readability. Betty style fix. Code passed all the checks

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,10 +1,17 @@
 #include "binary_trees.h"
-#include <stdlib.h>
+
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf
+ *
+ * @node: The node to be checked
+ *
+ * Return: 0 if it is not a leaf. 1 if it is a leaf
+ */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	/* according to main only check right nodes */
-	if (!node || (*node).right || (*node).left)
+	if (!node || node->right || node->left)
 		return (0);
 	return (1);
 }
