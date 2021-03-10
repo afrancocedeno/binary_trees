be7c38ff770ac2a2462029b2b3eca3ca085595de
Lucho <2450@holbertonschool.com>
2021-03-10 12:22:29 -0500

    Standard Library moved to headers file. Code passed all the checks.

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
  * binary_tree_is_root - function that checks if a given node is a root
@@ -7,6 +6,7 @@
  * @node: input pointer to the nod eto check.
  * Return: 1 if node is a root, otherwise 0.
  */
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	/* it happens when you set the recursion with parent member */
