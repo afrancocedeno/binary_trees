0bb5e91e1fbf8d9b7148f9fd931ed8239afecc5b
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-10 18:51:54 -0500

    new line needed

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
  * binary_tree_postorder - function that post-order traversal a BT.
@@ -10,12 +9,16 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
+
 	/* now recursion right subtree */
+
 	if ((*tree).left)
 		binary_tree_postorder((*tree).left, func);
+
 	/* now recursion right subtree */
 	if ((*tree).right)
 		binary_tree_postorder((*tree).right, func);
+
 	/* visit the root */
 	func((*tree).n);
 }
