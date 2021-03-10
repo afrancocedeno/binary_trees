d64152836768d6165799e43941f1e18ae32af23d
Lucho <2450@holbertonschool.com>
2021-03-10 16:24:19 -0500

    using an auxiliar function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,20 +2,30 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_counter = 0;
+	int height_value;
 
-	if (!tree)
+	height_value = height(tree);
+
+	return ((size_t)height_value);
+
+}
+
+int height(const binary_tree_t *node)
+{
+	int h_right = 0, h_left = 0, max = 0;
+
+	if (!node->left && !node->right)
 		return (0);
 
-	if (tree->right)
-	{
-		height_counter += 1;
-		binary_tree_height(tree->right);
-	}
-	if (tree->left)
-	{
-		height_counter += 1;
-		binary_tree_height(tree->left);
-	}
-	return (height_counter);
+	if (node->left)
+		h_left = height(node->left) + 1;
+
+	if (node->right)
+		h_right = height(node->right) + 1;
+
+	if (h_left >= h_right)
+		max = h_left;
+	else
+		max = h_right;
+	return (max);
 }
