b28cbd7d42f8ba23f2b531d26d7efcc664546e43
Lucho <2450@holbertonschool.com>
2021-03-11 20:02:31 -0500

    Documentation added for all the functions. Code passes all the checkers

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,15 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_perfect - Checks if a sub-tree is perfect.
+ *
+ * @tree: Node to check if the sub-tree is perfect
+ *
+ * Return: 1 if is perfect, 0 if not
+ */
+
 int binary_tree_is_perfect(const binary_tree_t *tree)
- {
+{
 	int height = _height3(tree), value;
 
 	if (_size(tree) == (_pow(2, height + 1) - 1))
@@ -10,9 +18,17 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		value = 0;
 
 	return (value);
- }
+}
 
- int _height3(const binary_tree_t *tree)
+/**
+ * _height3 - Computes the height through an auxiliar function.
+ *
+ * @tree: Node to compute the height from
+ *
+ * Return: The height
+ */
+
+int _height3(const binary_tree_t *tree)
 {
 	int height_value;
 
@@ -24,6 +40,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	return (height_value);
 
 }
+
+/**
+ * height_aux2 - Computes the height recursively
+ *
+ * @node: Node to compute the height from
+ *
+ * Return: The height
+ */
 
 int height_aux2(const binary_tree_t *node)
 {
@@ -45,6 +69,14 @@ int height_aux2(const binary_tree_t *node)
 	return (max);
 }
 
+/**
+ * _size - Computes the height recursively
+ *
+ * @tree: Node to compute the height from
+ *
+ * Return: The height
+ */
+
 int _size(const binary_tree_t *tree)
 {
 	/* If tree is NULL, do nothing */
@@ -53,6 +85,15 @@ int _size(const binary_tree_t *tree)
 	/* get size of left and right subtrees recursively if exist */
 	return (_size((*tree).left) + _size((*tree).right) + 1);
 }
+
+/**
+ * _pow - potentiation function
+ *
+ * @base: The base
+ * @exp: The exponent
+ *
+ * Return: The result of base ^ exp
+ */
 
 int _pow(int base, int exp)
 {
