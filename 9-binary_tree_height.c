a20f911ec19836c187aa2b5166f902158617d3cb
Lucho <2450@holbertonschool.com>
2021-03-11 19:04:49 -0500

    Betty Style fix. Code passed checkers

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,17 +1,33 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height - Computes the height of a tree
+ *
+ * @tree: The node to compute the height from
+ *
+ * Return: The height of the tree. 0 if NULL passed
+ */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int height_value;
 
 	if (!tree)
 		return (0);
-		
+
 	height_value = height(tree);
 
 	return ((size_t)height_value);
 
 }
+
+/**
+ * height - Auxiliar function for the recursive calling
+ *
+ * @node: The node to compute the height from
+ *
+ * Return: The height of the tree.
+ */
 
 int height(const binary_tree_t *node)
 {
