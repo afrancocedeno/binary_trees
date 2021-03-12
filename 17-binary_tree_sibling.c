ef19f12e6b6b9484cc6e37eaaa5f12a388e056df
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-11 21:28:08 -0500

    description header

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_sibling - function that finds the sibling of a node.
+ * @node: pointer to the node to find the sibling.
+ *
+ * Return: binary_tree_t datatype pointer to the sibling node.
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
